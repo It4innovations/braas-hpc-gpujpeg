f12f73ef30ad59d90d9aea7eacf0170ddf4d1851
Milan Jaros <jar091@vsb.cz>
2026-01-20 16:36:16 +0100

    add scripts for HIP/Win32

---
+++ scripts/hip/win32/run.cmd
@@ -0,0 +1,4 @@
+@echo off
+REM Build script for GPUJPEG using HIP/ROCm clang++ directly
+
+C:\Users\jar091\projects\braas-hpc-gpujpeg\build_hipcc\gpujpegtool.exe -n 10 -v --encode C:\temp\input.bmp C:\temp\input.jpg
